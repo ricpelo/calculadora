fc5f414bf2b711328e88e970dba248c54bb6b86f
Ricardo Pérez <ricpelo@gmail.com>
2018-10-19 21:20:42 +0200

    Calcula la suma

--- index.php
+++ index.php
@@ -5,14 +5,28 @@
         <title>Calculadora</title>
     </head>
     <body>
+        <?php
+        $op1 = isset($_GET['op1']) ? trim($_GET['op1']) : '0';
+        $op2 = isset($_GET['op2']) ? trim($_GET['op2']) : '0';
+        $op  = isset($_GET['op'])  ? trim($_GET['op'])  : '+';
+        $res = '';
+        ?>
         <form action="" method="get">
             <label for="op1">Primer operando:</label>
-            <input id="op1" type="text" name="op1"><br/>
+            <input id="op1" type="text" name="op1" value="<?= $op1 ?>"><br/>
             <label for="op2">Segundo operando:</label>
-            <input id="op2" type="text" name="op1"><br/>
+            <input id="op2" type="text" name="op2" value="<?= $op2 ?>"><br/>
             <label for="op">Operación:</label>
-            <input id="op" type="text" name="op"><br/>
+            <input id="op" type="text" name="op" value="<?= $op ?>"><br/>
             <input type="submit" value="Calcular">
         </form>
+        <?php
+        switch ($op) {
+            case '+':
+                $res = $op1 + $op2;
+                break;
+        }
+        ?>
+        <h3>Resultado: <?= $res ?></h3>
     </body>
 </html>
