b74174d4c43c8cd738e4d2fc8748825dffa7c16e
Ricardo Pérez <ricpelo@gmail.com>
2018-10-19 21:00:34 +0200

    Crea index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Calculadora</title>
+    </head>
+    <body>
+        <form action="" method="get">
+            <label for="op1">Primer operando:</label>
+            <input id="op1" type="text" name="op1"><br/>
+            <label for="op2">Segundo operando:</label>
+            <input id="op2" type="text" name="op1"><br/>
+            <label for="op">Operación:</label>
+            <input id="op" type="text" name="op"><br/>
+            <input type="submit" value="Calcular">
+        </form>
+    </body>
+</html>
