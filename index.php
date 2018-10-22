c2b40ab0eb52f21f87f6c9ae78dc95688c5ecbe6
Ricardo Pérez <ricpelo@gmail.com>
2018-10-22 19:19:07 +0200

    Comprobaciones de todo tipo

--- index.php
+++ index.php
@@ -8,23 +8,53 @@
         <?php
 
         const OP = ['+', '-', '*', '/'];
+        const PAR = ['op', 'op1', 'op2'];
 
         function selected($op1, $op2)
         {
             return $op1 == $op2 ? "selected" : "";
         }
 
-        $op1 = isset($_GET['op1']) ? trim($_GET['op1']) : '0';
-        $op2 = isset($_GET['op2']) ? trim($_GET['op2']) : '0';
-        $op  = isset($_GET['op'])  ? trim($_GET['op'])  : '+';
+        $op1 = $op2 = $op = null;
+        $error = [];
+
+        // Comprobación de parámetros:
+        $par = array_keys($_GET);
+        sort($par);
+        if (empty($_GET)) {
+            $op1 = '0';
+            $op2 = '0';
+            $op = '+';
+        } elseif ($par == PAR) {
+            $op1 = trim($_GET['op1']);
+            $op2 = trim($_GET['op2']);
+            $op = trim($_GET['op']);
+        } else {
+            $error[] = "Los parámetros recibidos no son los correctos.";
+        }
+
         $res = '';
+
+        if (empty($error)) {
+            // Comprobación de valores:
+            if (!is_numeric($op1)) {
+                $error[] = "El primer operando no es un número.";
+            }
+            if (!is_numeric($op2)) {
+                $error[] = "El segundo operando no es un número.";
+            }
+            if (!in_array($op, OP)) {
+                $error[] = "El operador no es válido.";
+            }
+        }
         ?>
+
         <form action="" method="get">
-            <label for="op1">Primer operando:</label>
+            <label for="op1">Primer operando *:</label>
             <input id="op1" type="text" name="op1" value="<?= $op1 ?>"><br/>
-            <label for="op2">Segundo operando:</label>
+            <label for="op2">Segundo operando *:</label>
             <input id="op2" type="text" name="op2" value="<?= $op2 ?>"><br/>
-            <label for="op">Operación:</label>
+            <label for="op">Operación *:</label>
             <select name="op">
                 <?php foreach (OP as $o): ?>
                     <option value="<?= $o ?>" <?= selected($op, $o) ?> >
@@ -34,22 +64,29 @@
             </select><br/>
             <input type="submit" value="Calcular">
         </form>
-        <?php
-        switch ($op) {
-            case '+':
-                $res = $op1 + $op2;
-                break;
-            case '-':
-                $res = $op1 - $op2;
-                break;
-            case '*':
-                $res = $op1 * $op2;
-                break;
-            case '/':
-                $res = $op1 / $op2;
-                break;
-        }
-        ?>
-        <h3>Resultado: <?= $res ?></h3>
+
+        <?php if (empty($error)): ?>
+            <?php
+            switch ($op) {
+                case '+':
+                    $res = $op1 + $op2;
+                    break;
+                case '-':
+                    $res = $op1 - $op2;
+                    break;
+                case '*':
+                    $res = $op1 * $op2;
+                    break;
+                case '/':
+                    $res = $op1 / $op2;
+                    break;
+            }
+            ?>
+            <h3>Resultado: <?= $res ?></h3>
+        <?php else: ?>
+            <?php foreach ($error as $err): ?>
+                <h3>Error: <?= $err ?></h3>
+            <?php endforeach ?>
+        <?php endif ?>
     </body>
 </html>
